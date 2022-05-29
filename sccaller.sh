2253f43921f860e795a8c918076cd6e32e6ed96f
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2022-05-29 14:19:20 +0800

    Update sccaller.sh

--- sccaller.sh
+++ sccaller.sh
@@ -2,3 +2,6 @@
 #including fastQC, trimgalore, mapping and sort
 #use hapllotype caller call germline mutation, also preformed BQSR, bamQC
 
+#run nf-core
+run_nfcore_haplotypecaller_vep.sh GRCh37 /Jan-Lab/zhengchen/nfscore/P13N/samplesheetp13N.tsv /Jan-Lab/zhengchen/new/P13N
+
