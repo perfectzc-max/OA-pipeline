c0a00d030345c893572710e822f6529b0c5fd96a
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2022-08-15 14:03:06 +0800

    Create filter2.sh

---
+++ filter/filter2.sh
@@ -0,0 +1,35 @@
+path=$1
+cell=$2
+
+currTime=$(date +"%Y-%m-%d %T")
+
+sleep 120
+
+echo ${path}\/${cell}\.vcf  $currTime
+#snv
+grep '0/1' ${path}\/${cell}\.vcf | grep 'True' | awk '$7=="." && length($5)==1 && $6>=30'  | awk -F "[:,]" '$8+$9>=20' | grep -v "X" | grep -v "Y" | grep -v "MT" > ${cell}\.snv.vcf
+
+#indel
+grep '0/1' ${path}\/${cell}\.vcf | grep 'True' | awk '$7=="." && length($5)>1 && $6>=25' | awk -F "[:,]" '$8+$9>=30'| grep -v "X" | grep -v "Y" | grep -v "MT" > ${cell}\.indel.vcf
+
+#header
+grep '#' ${path}\/${cell}\.vcf > ${cell}\.header.vcf
+
+#add header
+cat ${cell}\.header.vcf ${cell}\.snv.vcf > ${cell}\.snv.run.vcf
+cat ${cell}\.header.vcf ${cell}\.indel.vcf > ${cell}\.indel.run.vcf
+
+#dbsnp annotate
+java -jar /Software/Packages/snpEff/SnpSift.jar annotate /Jan-Lab/zhengchen/snpEff/ref/144-All.vcf.gz ${cell}\.snv.run.vcf >${cell}\.snv.dbsnp.vcf
+
+#filter dbsnp
+awk '$3== "." && length($1) <3' ${cell}\.snv.dbsnp.vcf >${cell}\.snv.ann.vcf
+
+#snpeff
+export NXF_SINGULARITY_CACHEDIR=/Software/nf_core/singularity_imgs/sarek_v2.7/
+/Software/nf_core/NEXTFLOW/nextflow run /Software/nf_core/nf-core-sarek-2.7/workflow/  --input ${cell}\.snv.ann.vcf --tools SnpEff  --step annotate  --outdir SnpEff_result  --genome GRCh37  --igenomes_base /Software/Refs/references  --species homo_sapiens  -profile singularity  -c /Software/nf_core/slurm_config/slurm_submit.config  -bg
+
+export NXF_SINGULARITY_CACHEDIR=/Software/nf_core/singularity_imgs/sarek_v2.7/
+/Software/nf_core/NEXTFLOW/nextflow run /Software/nf_core/nf-core-sarek-2.7/workflow/  --input ${cell}\.indel.run.vcf --tools SnpEff  --step annotate  --outdir SnpEff_result  --genome GRCh37  --igenomes_base /Software/Refs/references  --species homo_sapiens  -profile singularity  -c /Software/nf_core/slurm_config/slurm_submit.config  -bg
+
+echo ${cell} "done"
